05f89ec151aa5fc49d2be7e7b947d7f101ca9a2e
DeepaThangaraju <deepat2405@gmail.com>
2026-08-02 08:47:30 +0530

    posts

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -2,10 +2,10 @@ import express from "express";
 import { createPost, deletePost, getPost, getPosts, updatePost } from "../Controller/postController.js";
 const route = express.Router();
 
-route.post("/post", createPost);
-route.get("/post/:id", getPost);
+route.post("/posts", createPost);
+route.get("/posts/:id", getPost);
 route.get("/posts", getPosts);
-route.delete("/post/:id", deletePost);
-route.patch("/post/:id", updatePost);
+route.delete("/posts/:id", deletePost);
+route.patch("/posts/:id", updatePost);
 
 export default route;
